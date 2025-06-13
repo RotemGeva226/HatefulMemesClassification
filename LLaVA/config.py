61a27b6be9d396c79f83b5443c5e821ad41f3d5e
Rotem.Geva <rotem.geva@s.afeka.ac.il>
2025-06-13 20:29:36 +0300

    Add time parameter to config.py

--- LLaVA/config.py
+++ LLaVA/config.py
@@ -1,8 +1,10 @@
+from datetime import datetime
 from pathlib import Path
 import torch
 
 BASE_PATH = Path(r"C:\Users\rotem.geva\PycharmProjects\HatefulMemesClassification")
 RAW_DATA_DIR = BASE_PATH / "raw_data"
+CURR_TIME = datetime.now().strftime('%Y%m%d-%H%M%S')
 
 config = {
     "batch_size":1,
@@ -12,4 +14,5 @@ config = {
     "model_id": "llava-hf/llava-1.5-7b-hf",
     "device": "cuda" if torch.cuda.is_available() else "cpu",
     "train_path": RAW_DATA_DIR / "train.jsonl",
+    "time": CURR_TIME
 }
