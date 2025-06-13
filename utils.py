c25e75534bbd37920cb0ac010fa2b8f1c7dc54a1
Rotem.Geva <rotem.geva@s.afeka.ac.il>
2025-06-13 20:06:46 +0300

    Modify raw_data.csv to train_data.csv and Add collate_fn function to control batch structure

--- utils.py
+++ utils.py
@@ -2,11 +2,13 @@ import os
 import pandas as pd
 import json
 import kagglehub
+import torch
+
 
 def parse_jsonl_to_df(jsonl_path: str) -> None:
     """Parse and outputs json file to pandas dataframe"""
     base_directory = os.path.dirname(jsonl_path)
-    output_filepath = os.path.join(base_directory, 'raw_data.csv')
+    output_filepath = os.path.join(base_directory, 'train_data.csv')
 
     data = []
     with open(jsonl_path, 'r') as f:
@@ -20,6 +22,10 @@ def download_dataset(dataset_path: str = "parthplc/facebook-hateful-meme-dataset
     """Downloads a specified dataset from Kaggle Hub"""
     path = kagglehub.dataset_download(dataset_path)
     print(f"Dataset downloaded to: {path}")
+
+def collate_fn(batch):
+    images,prompts,labels = zip(*batch)
+    return list(images), list(prompts), torch.tensor(labels)
 
 if __name__ == "__main__":
     train_json_path = r"C:\Users\rotem.geva\PycharmProjects\HatefulMemesClassification\raw_data\train.jsonl"
