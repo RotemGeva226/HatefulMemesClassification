e09f4b156e1d9f5343ab9d9630221208f6ff4bec
Rotem.Geva <rotem.geva@s.afeka.ac.il>
2025-06-13 20:04:06 +0300

    Add config file for plug & play executions

---
+++ LLaVA/config.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+import torch
+
+BASE_PATH = Path(r"C:\Users\rotem.geva\PycharmProjects\HatefulMemesClassification")
+RAW_DATA_DIR = BASE_PATH / "raw_data"
+
+config = {
+    "batch_size":1,
+    "epochs": 3,
+    "learning_rate": 2e-5,
+    "project_name": "llava-meme-classifier",
+    "model_id": "llava-hf/llava-1.5-7b-hf",
+    "device": "cuda" if torch.cuda.is_available() else "cpu",
+    "train_path": RAW_DATA_DIR / "train.jsonl",
+}
