41d73c32cfc860636e8e4fd001bc73ad6fb779f1
Rotem.Geva <rotem.geva@s.afeka.ac.il>
2025-06-14 13:58:19 +0300

    Add support for validation data file

--- LLaVA/config.py
+++ LLaVA/config.py
@@ -13,6 +13,7 @@ config = {
     "project_name": "llava-meme-classifier",
     "model_id": "llava-hf/llava-1.5-7b-hf",
     "device": "cuda" if torch.cuda.is_available() else "cpu",
-    "data_path": RAW_DATA_DIR / "train.jsonl",
+    "train_path": RAW_DATA_DIR / "train.jsonl",
+    "validation_path": RAW_DATA_DIR / "dev.jsonl",
     "time": CURR_TIME
 }
