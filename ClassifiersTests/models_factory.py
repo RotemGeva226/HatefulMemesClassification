71999ae7801b032f5fb276558a6f4d75183dae04
Rotem.Geva <rotem.geva@s.afeka.ac.il>
2025-07-21 12:15:05 +0300

    Add support for xgboost

--- ClassifiersTests/models_factory.py
+++ ClassifiersTests/models_factory.py
@@ -1,5 +1,15 @@
 from sklearn.neural_network import MLPClassifier
+import xgboost as xgb
 
 def create_classifier(name, params):
     if name == 'mlp':
+        '''params={
+            "hidden_layer_sizes": [512, 256],
+            "early_stopping": True,
+            "n_iter_no_change": 10,
+            "max_iter": 300,
+        }'''
         return MLPClassifier(**params)
+    if name == 'xgboost':
+        return xgb.XGBClassifier(**params)
+
